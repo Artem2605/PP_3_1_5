0e97675244f8a90de5c70ecd9f9dabf5fbea9d1b
Artem2605 <artyom260599@mail.ru>
2023-11-30 21:48:08 +0300

    1

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -11,7 +11,6 @@ import java.security.Principal;
 public class UserController {
 
     private final UserServiceImpl userService;
-    //
 
     @Autowired
     public UserController(UserServiceImpl userService) {
