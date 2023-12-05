bf92d4c4a4b97f768b48d7b3688d43accc3c414a
Artem2605 <artyom260599@mail.ru>
2023-12-05 04:05:15 +0300

    1

--- src/main/resources/static/temp.js
+++
@@ -1,8 +0,0 @@
-async function getAllUsers() {
-    const res = await fetch('http://localhost:8080/api/users');
-    const users = await res.json();
-
-    console.log(users);
-}
-
-window.addEventListener('DOMContentLoaded', getAllUsers)
